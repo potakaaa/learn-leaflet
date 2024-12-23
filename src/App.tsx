a426cf5f94af495c1533f511b65072c07b636cbb
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2024-12-23 20:31:32 +0800

    m

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Map from "./components/Map";
 const App: React.FC = () => {
   return (
     <div className="size-full">
-      <h1>Jeepney route navigation shit</h1>
+      <h1>Jeepney route navigation </h1>
       <Map />
     </div>
   );
