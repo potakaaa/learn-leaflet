d063eb889c36fae2e53bcfb775832ad921c15b09
potakaaa <helbirog@gmail.com>
2024-12-25 18:59:08 +0800

    initialized maptiler

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,16 +7,16 @@ import {
   Marker,
   LayersControl,
 } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import "leaflet-routing-machine";
-import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
-import "leaflet-routing-machine/dist/leaflet-routing-machine.js";
-import L, { Routing } from "leaflet";
-import { useEffect } from "react";
-import { useMap } from "react-leaflet";
+import { useState } from "react";
+import OSM from "../providers/osm-provider";
 
 const Map = () => {
-  const defaultCenter: [number, number] = [8.47543, 124.64212];
+  // USTP Coordinates
+  const [center, setCenter] = useState({
+    lat: 124.65665999809528,
+    lng: 8.484850986252226,
+  });
+  const zoomLevel = 13;
 
   const start: [number, number] = [8.501678, 124.632554];
   const end: [number, number] = [8.484751, 124.63411];
@@ -119,150 +119,18 @@ const Map = () => {
     },
   ];
 
-  const calculateNearestPointOnRoute = (
-    startPoint: [number, number],
-    route: [number, number][]
-  ): [number, number] => {
-    let minDistance = Infinity;
-    let nearestPoint: [number, number] | null = null;
-
-    for (let i = 0; i < route.length - 1; i++) {
-      const pointA = route[i];
-      const pointB = route[i + 1];
-
-      const closestPoint = calculateClosestPointOnSegment(
-        startPoint,
-        pointA,
-        pointB
-      );
-
-      const distance = Math.sqrt(
-        Math.pow(closestPoint[0] - startPoint[0], 2) +
-          Math.pow(closestPoint[1] - startPoint[1], 2)
-      );
-
-      if (distance < minDistance) {
-        minDistance = distance;
-        nearestPoint = closestPoint;
-      }
-    }
-
-    return nearestPoint!;
-  };
-  const calculateClosestPointOnSegment = (
-    point: [number, number],
-    segmentStart: [number, number],
-    segmentEnd: [number, number]
-  ): [number, number] => {
-    const [x1, y1] = segmentStart;
-    const [x2, y2] = segmentEnd;
-    const [px, py] = point;
-
-    const dx = x2 - x1;
-    const dy = y2 - y1;
-
-    const t = ((px - x1) * dx + (py - y1) * dy) / (dx * dx + dy * dy);
-
-    const clampedT = Math.max(0, Math.min(1, t));
-
-    return [x1 + clampedT * dx, y1 + clampedT * dy];
-  };
-
-  const nearestPoint = calculateNearestPointOnRoute(
-    start,
-    jeepneyRoutes[0].coordinates
-  );
-
   return (
     <MapContainer
-      center={defaultCenter}
-      zoom={13}
+      center={center}
+      zoom={zoomLevel}
       style={{ height: "500px", width: "100%" }}
     >
-      <LayersControl position="topright">
-        <LayersControl.BaseLayer checked name="Default OpenStreetMap">
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-        </LayersControl.BaseLayer>
-
-        <LayersControl.BaseLayer name="CartoDB Light">
-          <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-          />
-        </LayersControl.BaseLayer>
-
-        <LayersControl.Overlay checked name="Jeepney Routes">
-          <>
-            {jeepneyRoutes.map((route, index) => (
-              <Polyline
-                key={index}
-                positions={route.coordinates}
-                color={route.color}
-                weight={4}
-              >
-                <Tooltip permanent>{route.name}</Tooltip>
-              </Polyline>
-            ))}
-          </>
-        </LayersControl.Overlay>
-
-        {nearestPoint && (
-          <LayersControl.Overlay checked name="Routing">
-            <RoutingControl start={start} end={nearestPoint} />
-          </LayersControl.Overlay>
-        )}
-      </LayersControl>
+      <TileLayer
+        url={OSM.maptiler.url}
+        attribution={OSM.maptiler.attribution}
+      ></TileLayer>
     </MapContainer>
   );
 };
 
 export default Map;
-
-const RoutingControl = ({
-  start,
-  end,
-}: {
-  start: [number, number];
-  end: [number, number];
-}) => {
-  const map = useMap();
-
-  useEffect(() => {
-    if (!map) return;
-
-    const routingControl = L.Routing.control({
-      waypoints: [L.latLng(start[0], start[1]), L.latLng(end[0], end[1])],
-      routeWhileDragging: false,
-      showAlternatives: false,
-      lineOptions: {
-        styles: [{ color: "black", dashArray: "5,5", weight: 4 }],
-        extendToWaypoints: true,
-        missingRouteTolerance: 100,
-      },
-    }).addTo(map);
-
-    routingControl.getWaypoints().forEach((waypoint, index) => {
-      const latLng = waypoint.latLng;
-      if (latLng) {
-        const marker = L.marker(latLng, {
-          draggable: index === 0,
-        }).addTo(map);
-
-        if (index === 0) {
-          marker.bindPopup("Start Point").openPopup();
-        } else if (index === 1) {
-          marker.bindPopup("End Point").openPopup();
-        }
-      }
-    });
-
-    return () => {
-      map.removeControl(routingControl);
-    };
-  }, [map, start, end]);
-
-  return null;
-};
---
+++ src/providers/osm-provider.tsx
@@ -0,0 +1,9 @@
+const OSM = {
+  maptiler: {
+    url: "https://api.maptiler.com/maps/satellite/256/{z}/{x}/{y}.jpg?key=SlZNxUiHmBSoWZ1YUoLb",
+    attribution:
+      '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+  },
+};
+
+export default OSM;
